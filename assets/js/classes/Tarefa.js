7ac93994fbb52cec5c37467719124a1f7d0553b6
Filipe Góis <fythiy@gmail.com>
2024-06-30 14:44:35 -0300

    modal implementado. Terminar de estilizar seus componentes filhos

--- assets/js/classes/Tarefa.js
+++ assets/js/classes/Tarefa.js
@@ -1,7 +1,14 @@
-class Tarefa {
+export class Tarefa {
   constructor(nome, descricao, data) {
     this.nome = nome;
     this.descricao = descricao;
     this.data = data;
+    this.favoritada = false;
+  }
+  favoritar() {
+    this.favoritada = true;
+  }
+  desfavoritar() {
+    this.favoritada = false;
   }
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,49 @@
 // <!-- adicionados via js -->
 //HTML
 
+import { Tarefa } from "./classes/Tarefa.js";
+
+const urlApi = ``;
+
+const fadeDiv = document.querySelector("#fade");
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".btn-abrir-modal");
+const filtroSelecionadoClasse = "filtro-selecionado";
+const filtrosItens = [...document.querySelectorAll(".filtro-conteudo")];
+
+const toggleModal = () =>
+  [modal, fadeDiv].map((e) => e.classList.toggle("hide"));
+
+[openModal, fadeDiv].map((el) =>
+  el.addEventListener("click", () => toggleModal())
+);
+
+//lógica para alterar filtro de tarefas
+filtrosItens.map((filtro) => {
+  filtro.addEventListener("click", (e) => {
+    filtrosItens.map((item) => item.classList.remove(filtroSelecionadoClasse));
+
+    filtro.classList.add(filtroSelecionadoClasse);
+  });
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   const themeSwitcher = document.querySelector(".my-body");
 
   themeSwitcher.addEventListener("change", (e) => {
     document.body.classList.toggle("light-theme");
-    console.log(document.body.classList);
   });
 });
+
+const adicionarTarefa = async () => {
+  try {
+    // const novaTarefa = new Tarefa();
+    // await fetch(urlApi, { method: "POST", body: novaTarefa });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const t1 = new Tarefa("sla", "sakjhdasd", "14/01/2007");
+t1.favoritar();
+console.log(t1);
